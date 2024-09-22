867e7e08b04428f0459ac2036961a4e908a9720c
Sanjumore07 <sanjivanimore2605@gmail.com>
2024-09-22 22:57:15 +0530

    Add files via upload

---
+++ if else.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ int number=234;
+ 
+ //ccheck if the number is greater than 5
+ if(number>5)
+ {
+   cout<<"The number it greater than 5"<<endl;
+ }
+ else
+ {
+   cout<<"The number us 5 or less"<<endl;
+   }
+   return 0;
+ }
